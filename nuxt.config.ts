287e9af620783e8648e718958b33bc5284c5498c
Admin <admin@mondragonmexico.edu.mx>
2025-09-18 14:37:22 -0600

    Actualización de NuxtUI
    - Agregado animate.css y estilos de TailwindCSS
    - Agregadas imágenes de negocio

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,9 +3,31 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
 
+  app: {
+    head: {
+      title: 'Universidad Mondragón México. Campus Querétaro.',
+      meta: [
+        { name: 'description', content: 'Universidad Mondragón México. Campus Querétaro' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { name: 'format-detection', content: 'telephone=no' },
+      ],
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+        { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
+        { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: '' },
+        { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap' },
+      ],
+    },
+  },
+
   modules: [
     '@nuxt/image',
     '@nuxt/scripts',
     '@nuxt/ui',
+  ],
+
+  css: [
+    '~/assets/css/main.css',
+    'animate.css/animate.min.css',
   ],
 });
